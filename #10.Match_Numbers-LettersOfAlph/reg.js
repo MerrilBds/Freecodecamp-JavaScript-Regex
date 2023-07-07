8b28cce335fbe66f9720ac9f7177b294104e1912
MerrilBds <Bdsmerril@gmail.com>
2023-07-07 16:12:16 +0200

    leçon 10 fin

---
+++ #10.Match_Numbers-LettersOfAlph/reg.js
@@ -0,0 +1,13 @@
+/*
+Match Numbers and Letters of the Alphabet
+
+Using the hyphen (-) to match a range of characters is not limited to letters. 
+It also works to match a range of numbers.
+
+For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
+
+Also, it is possible to combine a range of letters and numbers in a single character set
+*/
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig;
+let result = quoteSample.match(myRegex); 
