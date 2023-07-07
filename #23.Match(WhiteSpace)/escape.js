97ede0557b81636b7d68f0e82e42ed090dc291be
MerrilBds <Bdsmerril@gmail.com>
2023-07-07 17:26:08 +0200

    leçon 23 fin

---
+++ #23.Match(WhiteSpace)/escape.js
@@ -0,0 +1,4 @@
+//you can search for whitespace using \s, which is a lowercase 
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g;
+let result = sample.match(countWhiteSpace);
